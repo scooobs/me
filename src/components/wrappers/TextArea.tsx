c75d6f439110afa4aa7a6ba458ab67712e63b1b4
Conal O'Leary <52145084+scooobs@users.noreply.github.com>
2023-06-26 21:40:16 +0800

    Don't use arrow functions dummy

--- src/components/wrappers/TextArea.tsx
+++ src/components/wrappers/TextArea.tsx
@@ -2,19 +2,23 @@ import React from "react";
 
 export const TextArea: React.FC<
   Omit<React.HTMLProps<HTMLTextAreaElement>, "ref" | "onChange">
-> = (props) => (
-  <textarea
-    {...props}
-    ref={(e) => {
+> = (props) => {
+  const refCallback: React.RefCallback<HTMLTextAreaElement> = React.useCallback(
+    (e) => {
       if (e == null) {
         return;
       }
       e.style.height = "0";
       e.style.height = e.scrollHeight.toString() + "px";
-    }}
-    onChange={(e) => {
+    },
+    []
+  );
+
+  const onChangeCallback: React.ChangeEventHandler<HTMLTextAreaElement> =
+    React.useCallback((e) => {
       e.target.style.height = "0";
       e.target.style.height = e.target.scrollHeight.toString() + "px";
-    }}
-  />
-);
+    }, []);
+
+  return <textarea {...props} ref={refCallback} onChange={onChangeCallback} />;
+};
